1195a56cd53325a132478d393d4e3a03ab3613a4
Adithya KLN <adithyakarlapalem@gmail.com>
2017-05-10 13:27:19 -0700

    Added Class to parse Units based Questions

--- app/app/class_info.py
+++ app/app/class_info.py
@@ -17,3 +17,19 @@ class ClassPrerequisiteQuestion(QuestionTemplate):
         prerequisite = IsClassRelated() + match.course + HasFields('prerequisites'.decode('utf-8')) \
                        + HasAnswer(answer.decode('utf-8'))
         return prerequisite
+
+
+class UnitsQuestion(QuestionTemplate):
+    """
+        Ex: "How many units are there for cmpe 273?"
+            How many credits are there for cmpe 287?
+            What number of units are there for cmpe 273?
+    """
+    regex = Lemmas("what be") + Lemma("number") + Question(Pos("DT")) + Lemma("units") +Pos("IN") + Course() + Question(Pos("."))
+            # Lemma("who") + Lemma("teach") + Course() + Question(Pos("."))| \
+            # Pos("IN") + Lemmas("whom be") + Course() + Lemma("taught") + Question(Pos("."))
+
+    def interpret(self, match):
+        answer = "The units awarded for %s is %s"
+        units = IsClassRelated() + match.course + HasFields('units'.decode('utf-8')) + HasAnswer(answer.decode('utf-8'))
+        return units
